28b5fcad3581c8fd01cd0c9d881801468a8f4571
Andrey Antukh <niwi@niwi.nz>
2016-06-22 17:25:14 +0300

    Improve build/watch scripts.

--- scripts/build.clj
+++ scripts/build.clj
@@ -5,13 +5,14 @@
 (let [start (System/nanoTime)]
   (b/build
    (b/inputs "test" "src")
-   {:main 'lentes.test
+   {:main 'lentes.tests
+    :parallel-build false
     :output-to "out/tests.js"
-    :output-dir "out"
+    :output-dir "out/tests"
     :target :nodejs
     :optimizations :none
     :pretty-print true
-    :language-in  :ecmascript5
+    :language-in  :ecmascript6
     :language-out :ecmascript5
     :verbose true})
   (println "... done. Elapsed" (/ (- (System/nanoTime) start) 1e9) "seconds"))
--- scripts/watch.clj
+++ scripts/watch.clj
@@ -1,12 +1,14 @@
 (require '[cljs.build.api :as b])
 
-(b/watch (b/inputs "test" "src")
-  {:main 'lentes.tests
-   :target :nodejs
-   :output-to "out/tests.js"
-   :output-dir "out"
-   :optimizations :none
-   :pretty-print true
-   :language-in  :ecmascript5
-   :language-out :ecmascript5
-   :verbose true})
+(b/watch
+ (b/inputs "test" "src")
+ {:main 'lentes.tests
+  :parallel-build false
+  :output-to "out/tests.js"
+  :output-dir "out/tests"
+  :target :nodejs
+  :optimizations :none
+  :pretty-print true
+  :language-in  :ecmascript6
+  :language-out :ecmascript5
+  :verbose true})
